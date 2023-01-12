64155b5f8be2a1ebe9e6d53cdbe536427a31527f
KSA1978KSA <s.kurochkin@itfbgroup.ru>
2023-01-12 15:26:53 +0300

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "./build",
+  },
+  base: "/vite-react-trainer-people-memory/",
   plugins: [react()],
-})
+});
